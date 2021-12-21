5cba6e21f3d72d1b3bf79b3a72ea58528efeade0
John Young <john@codeandcreative.com>
2021-12-20 17:07:56 -0800

    Changes build to just use webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,39 @@
-const path = require('path');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  entry: './src/js/index.js',
+  entry: ["./src/js/index.js", "./src/scss/starter.scss"],
   output: {
-    filename: 'starter.min.js',
-    path: path.resolve(__dirname, 'build/js'),
+    filename: "js/starter.min.js",
+    path: path.resolve(__dirname, "build"),
   },
   plugins: [
     new CopyWebpackPlugin({
-        patterns: [
-            { from: 'static', to: path.resolve(__dirname, 'build') }
-        ]
-    })
-]
+      patterns: [
+        { from: "./src/img", to: path.resolve(__dirname, "build/img") },
+      ],
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "file-loader",
+            options: { outputPath: "css", name: "[name].min.css" },
+          },
+
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  },
 };
